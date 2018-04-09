95617a71ffc7cdb14116ab9c1c4445ed9bec6a50
Nicholas Powers <njpowers7915@gmail.com>
2018-04-09 18:01:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,6 +32,5 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
-  return array;
+  return array.slice(1);
 }
