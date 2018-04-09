ce9100d0214175909cd38fdf8b714c2e78e98c65
Nicholas Powers <njpowers7915@gmail.com>
2018-04-09 16:48:04 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,24 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+var array = [1]
+var element = 'foo'
+function addElementToBeginningOfArray(array, element) {
+  var newArray = array.unshift(element);
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array = [element, ...array];
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  var newArray = array.push(element);
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array = [...array, element];
+  return array;
+}
+
